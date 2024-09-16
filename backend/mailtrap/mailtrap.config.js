0a58aa0f8329e29bf90e621b003ce41e0aa36320
taietor <84741399+taietor@users.noreply.github.com>
2024-09-16 12:15:59 +0200

    first commit

---
+++ backend/mailtrap/mailtrap.config.js
@@ -0,0 +1,11 @@
+import { MailtrapClient } from "mailtrap";
+
+
+export const mailtrapClient = new MailtrapClient({
+	token: process.env.MAILTRAP_TOKEN,
+});
+
+export const sender = {
+	email: "mailtrap@demomailtrap.com",
+	name: "taietor",
+};
